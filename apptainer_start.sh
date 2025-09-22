87b33d133c7bda225ad4d7ae74e418fb41fa0a93
Kevin Shih <Kevin-Shih@users.noreply.github.com>
2025-09-22 13:38:12 +0800

    Feat: Default Hostname to Instance Name.
    Set hostname as third argument when starting new instance.
    If not provided, default to instance name.

--- apptainer_start.sh
+++ apptainer_start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <container_image> [instance_name]"
+    echo "Usage: $0 <container_image> [instance_name] [host_name]"
     exit 1
 fi
 
 # Use second argument as instance name if provided, otherwise default to "ub24"
 INSTANCE_NAME="${2:-ub24}"
+# Use third argument as host name, otherwise default to instance name.
+HOST_NAME="${3:-$INSTANCE_NAME}"
 
-
-
-apptainer instance start --nv \
+apptainer instance start --nv --hostname "$HOST_NAME" \
     -B /eva_data0:/eva_data0 \
     -B /eva_data1:/eva_data1 \
     -B /eva_data2:/eva_data2 \
